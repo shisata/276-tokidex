38588154c264fe6d8cc11cda288115f92bf0c779
qttran <qttran@sfu.ca>
2019-10-04 13:54:00 -0700

    set up

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
+  .get('/', (req, res) => res.sendfile('public/home.html'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
