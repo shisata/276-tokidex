1b1a81d08a2873811b5d20383d34e2f9f29cec14
qttran <qttran@sfu.ca>
2019-10-14 14:51:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ express()
   //.use(express.json());
   //.use(express.urlencoded({extended:false}));
   .set('views', path.join(__dirname, 'views'));
-  .get('/', (req, res) => {
-    res.sendfile('public/home.html');
-
-    })
+  .get('/', (req, res) => res.sendfile('public/home.html');
   .get('/times', (req, res) => res.send(showTimes()))
   .get('/db', async (req, res) => {
     try {
