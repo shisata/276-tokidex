b03653470956dab05bf0ac08baa5f87780f10d87
qttran <qttran@sfu.ca>
2019-10-14 15:06:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const pool = new Pool({
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  //.use(express.json());
-  //.use(express.urlencoded({extended:false}));
+  .use(express.json())
+  .use(express.urlencoded({extended:false}))
   .set('views', path.join(__dirname, 'views'))
   .get('/', (req, res) => res.sendfile('public/home.html'))
   .get('/times', (req, res) => res.send(showTimes()))
