11d171a89b3652336457232c4b40d14dc1969dea
qttran <qttran@sfu.ca>
2019-10-15 12:14:22 -0700

    Update script.js

--- views/script.js
+++ views/script.js
@@ -1,13 +1,4 @@
 
-function alertPage(results){
-  alert(results);
-}
-
-function insertElement(array, element){
-  alert("insertElement()");
-  array.push(element);
-}
-
 function reloadPage(){
   location.reload();
 }
