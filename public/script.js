1bd1a11bb4c4a38afed93135a2a98495415ff01a
qttran <qttran@sfu.ca>
2019-10-15 12:17:29 -0700

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,14 @@
+var tokiList = [];
+
+function alertPage(results){
+  alert(results);
+}
+
+function insertElement(array, element){
+  alert("insertElement()");
+  array.push(element);
+}
+
+function reloadPage(){
+  location.reload();
+}
