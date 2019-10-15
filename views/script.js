1cfda4686ccd438e05ec38c23f7510807f10b08c
qttran <qttran@sfu.ca>
2019-10-15 12:15:53 -0700

    a

--- views/script.js
+++ views/script.js
@@ -1,3 +1,13 @@
+var tokiList = [];
+
+function alertPage(results){
+  alert(results);
+}
+
+function insertElement(array, element){
+  alert("insertElement()");
+  array.push(element);
+}
 
 function reloadPage(){
   location.reload();
