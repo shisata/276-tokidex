8ef2ffb25d3a68f2528bd911c07bcc488843e6e9
qttran <qttran@sfu.ca>
2019-10-15 19:31:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ express()
       res.send("Error " + err);
     }
   })
-  .get('/id', (req, res) =>{
+  .get('/detail', async (req, res) =>{
     try{
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM tokidex');
