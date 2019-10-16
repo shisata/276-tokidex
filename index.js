9e95765b000d9249cc4094adf33a00c78e65c829
qttran <qttran@sfu.ca>
2019-10-15 21:10:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -66,25 +66,20 @@ express()
       var trainer = list.trainer;
       var total = 0;
 
-      // const result = await client.query('INSERT INTO tokidex ' +
-      // '(name, weight, height, element1, power1, element2, power2, element3, power3, trainer, total) VALUES ( ' +
-      // name + ', ' +
-      // weight + ', ' +
-      // height + ', ' +
-      // element1 + ', ' +
-      // power1 + ', ' +
-      // element2 + ', ' +
-      // power2 + ', ' +
-      // element3 + ', ' +
-      // power3 + ', ' +
-      // trainer + ', ' +
-      // total + ')' );
+      const result = await client.query('INSERT INTO tokidex ' +
+      '(name, weight, height, element1, power1, element2, power2, element3, power3, trainer, total) VALUES ( ' +
+      name + ', ' +
+      weight + ', ' +
+      height + ', ' +
+      element1 + ', ' +
+      power1 + ', ' +
+      element2 + ', ' +
+      power2 + ', ' +
+      element3 + ', ' +
+      power3 + ', ' +
+      trainer + ', ' +
+      total + ')' );
 
-      const result = await client.query("insert into tokidex (name, weight, height, element1, power1, element2, power2, element3, power3, trainer, total) values ('Raiku', 50000, 321321, 'fight', 9000, 'water', 521, 'fire', 521, 'POKECHU', 99999);");
-
-
-      // const result = await client.query("SELECT * FROM tokidex");
-      // const results = {'results': (result) ? result.rows : null};
       res.render('create');
       client.release();
     } catch(err){
